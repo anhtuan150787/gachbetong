da5b604834a3bbb1a05718a34509a9a254a02d44
Anh Tuan <anhtuan150787@gmail.com>
2018-10-09 14:11:41 +0700

    Update

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php ini_set('display_errors', 'off');
+<?php ini_set('display_errors', 'on');
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
